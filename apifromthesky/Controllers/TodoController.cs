8135a12ea91233d0ed8382fc481365f9c1929ada
lbilde <ljb@easv.dk>
2017-05-14 09:41:41 +0200

    adding Create Update Delete

--- apifromthesky/Controllers/TodoController.cs
+++ apifromthesky/Controllers/TodoController.cs
@@ -35,6 +35,53 @@ namespace apifromthesky.Controllers
 			}
 			return new ObjectResult(item);
 		}
+
+		[HttpPost]
+		public IActionResult Create([FromBody] TodoItem item)
+		{
+			if (item == null)
+			{
+				return BadRequest();
+			}
+
+			_todoRepository.Add(item);
+
+			return CreatedAtRoute("GetTodo", new { id = item.Key }, item);
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult Update(long id, [FromBody] TodoItem item)
+		{
+			if (item == null || item.Key != id)
+			{
+				return BadRequest();
+			}
+
+			var todo = _todoRepository.Find(id);
+			if (todo == null)
+			{
+				return NotFound();
+			}
+
+			todo.IsComplete = item.IsComplete;
+			todo.Name = item.Name;
+
+			_todoRepository.Update(todo);
+			return new NoContentResult();
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(long id)
+		{
+			var todo = _todoRepository.Find(id);
+			if (todo == null)
+			{
+				return NotFound();
+			}
+
+			_todoRepository.Remove(id);
+			return new NoContentResult();
+		}
     }
 
 	
